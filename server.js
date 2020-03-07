2b00badb775814ddc04ca036d7a48415d6f767f9
Bob Bass <bob@bbass.co>
2020-03-06 20:49:45 -0500

    added EJS functionality calling URLs

--- server.js
+++ server.js
@@ -14,17 +14,19 @@ app.set('view engine', 'ejs')
 // Set ShortUrl Setting
 app.use(express.urlencoded({ extended: false }))
 
-// // Routing
-app.get('/', (req, res) => {
-    // response, render index file
-    res.render('index')
+// // Routing - This is where we pass URLs into body
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    // response, render index file - pass in fullUrls into body
+    res.render('index', { shortUrls: shortUrls })
 })
 
 // From form post
 // Call the fullUrl property in the body of the EJS file
-    app.post('/shortUrls', async (req, res) => {
-        await ShortUrl.create({ full: req.body.fullUrl })
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
 
+    // Create new Short URL and redirect user home
     res.redirect('/')
 })
 
